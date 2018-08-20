cabfbc7efdf98cb13c3e0af17b462a9c3ba7aa64
Adam Johnson <adamwjo@gmail.com>
2018-08-20 15:59:10 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -24,7 +24,14 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  count = 0
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
 end
 
 def get_occupation(data, hometown)
