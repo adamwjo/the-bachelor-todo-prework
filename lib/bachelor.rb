d5e1d53caa13cb4029a26513c89451d21c6775ca
Adam Johnson <adamwjo@gmail.com>
2018-08-20 14:40:05 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,7 @@ def get_first_name_of_season_winner(data, season)
           first_name = value["name"]
     end
   end
-  first_name.split(" ")
+  new_name = first_name.split()
 end
 
 def get_contestant_name(data, occupation)
