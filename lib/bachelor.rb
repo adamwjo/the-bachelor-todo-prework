234cbe8cf80ec9928e9e53580be25d7b122bfe3e
Adam Johnson <adamwjo@gmail.com>
2018-08-20 14:50:04 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,7 +12,12 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name == nil
+  data.each do |key, value|
+    if data[key][value] == occupation
+      name = data[key][value]
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
