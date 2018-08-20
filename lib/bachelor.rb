6864e328363825de843c81c352365979be5c516c
Adam Johnson <adamwjo@gmail.com>
2018-08-20 16:53:22 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,59 @@
+require "pry"
 def get_first_name_of_season_winner(data, season)
-  # code here
+  first_name = nil
+  data[season].each do |value|
+        if value["status"] == "Winner"
+          first_name = value["name"]
+    end
+  end
+  new_name = first_name.split()
+  new_name.pop
+  new_name.join
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  new_name = nil
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["occupation"] == occupation
+        new_name = hash["name"]
+      end
+    end
+  end
+  new_name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = nil
+  data.each do |season, array|
+    array.each do |hash|
+      if hash["hometown"] == hometown
+        occupation = hash["occupation"]
+        return occupation
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average = 0
+  ages = 0
+  data[season].each do |value|
+    average += value["age"].to_f
+    ages += 1
+  end
+  average_age = average / ages
+  average_age.round
 end
