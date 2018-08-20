22a9c2a43c2304b99206e5c4b8443965f7ceadf2
Adam Johnson <adamwjo@gmail.com>
2018-08-20 13:08:48 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
   data.collect do |key, value|
     if key == season
       value.collect do |key, value|
-        if season[key] == "winner"
+        if season[key][value] == "winner"
           first_name = season[attribute]["name"]
         end
       end
